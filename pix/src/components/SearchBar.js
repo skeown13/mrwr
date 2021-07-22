609295201cbe13a2a918dc650042486785f4c667
skeown13 <code@keown.net>
2021-07-22 15:22:37 -0700

    created an uncontrolled element of a searchbar.

--- pix/src/components/SearchBar.js
+++ pix/src/components/SearchBar.js
@@ -1,13 +1,25 @@
 import React from "react"
+import { Input, Segment, Form, Label } from "semantic-ui-react"
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value)
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" id="searchMe" placeholder="Whatcha want?" />
-        </form>
-      </div>
+      <Segment>
+        <Form>
+          <Form.Field>
+            <Label>Image Search</Label>
+            <Input
+              placeholder="Whatcha want?..."
+              onChange={this.onInputChange}
+            />
+            {/* If we put () at end of onInputChange it would automatically call every time rendered. By leaving them off (referencing) it allows for it to be called at some point in the future. It is still a callback function. */}
+          </Form.Field>
+        </Form>
+      </Segment>
     )
   }
 }
