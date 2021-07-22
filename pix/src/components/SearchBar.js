5e2514bdf96bafbf5e84b02995d12197358869de
skeown13 <code@keown.net>
2021-07-22 16:42:41 -0700

    refactored uncontrolled searchbar to a controlled searchbar by using state

--- pix/src/components/SearchBar.js
+++ pix/src/components/SearchBar.js
@@ -2,9 +2,7 @@ import React from "react"
 import { Input, Segment, Form, Label } from "semantic-ui-react"
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value)
-  }
+  state = { term: "" }
 
   render() {
     return (
@@ -14,7 +12,8 @@ class SearchBar extends React.Component {
             <Label>Image Search</Label>
             <Input
               placeholder="Whatcha want?..."
-              onChange={this.onInputChange}
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
             />
             {/* If we put () at end of onInputChange it would automatically call every time rendered. By leaving them off (referencing) it allows for it to be called at some point in the future. It is still a callback function. */}
           </Form.Field>
