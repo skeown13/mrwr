14ebb83ddb5d07e0ea9fae35463d93ad6fcc1919
skeown13 <code@keown.net>
2021-07-25 11:58:11 -0700

    imported axios, made App into a class component, getting response from api

--- pix/src/components/App.js
+++ pix/src/components/App.js
@@ -1,12 +1,31 @@
 import React from "react"
+import axios from "axios"
 import SearchBar from "./SearchBar"
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: {
+          query: term,
+        },
+        headers: {
+          Authorization:
+            "Client-ID iY-rexw_XiE5jnJfhkvoegrb5anpWwY6BVjxTgmxL_0",
+        },
+      })
+      .then(function (res) {
+        console.log(res)
+      })
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    )
+  }
 }
 
 export default App
