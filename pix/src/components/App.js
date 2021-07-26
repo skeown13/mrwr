542f493053d18f682e5f987e46eda5d3ab49b7b3
skeown13 <code@keown.net>
2021-07-26 14:25:30 -0700

    added state to the class App component. created an onSearchSubmit async function

--- pix/src/components/App.js
+++ pix/src/components/App.js
@@ -1,28 +1,27 @@
 import React from "react"
-import axios from "axios"
+import unsplash from "../api/unsplash"
 import SearchBar from "./SearchBar"
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: {
-          query: term,
-        },
-        headers: {
-          Authorization:
-            "Client-ID iY-rexw_XiE5jnJfhkvoegrb5anpWwY6BVjxTgmxL_0",
-        },
-      })
-      .then(function (res) {
-        console.log(res)
-      })
+  state = {
+    images: [],
+  }
+
+  onSearchSubmit = async term => {
+    const res = await unsplash.get("/search/photos", {
+      params: {
+        query: term,
+      },
+    })
+
+    this.setState({ images: res.data.results })
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
       </div>
     )
   }
