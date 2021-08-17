ee55ceb4d9d32c2527f0ba199fbe6ab50e8c9354
skeown13 <code@keown.net>
2021-08-17 14:23:39 -0700

    added a piece of state for the selected video. created an onVideoSelect function that we passed to the VideoList Component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar"
 import VideoList from "./VideoList"
 
 class App extends React.Component {
-  state = { videos: [] }
+  state = { videos: [], selectedVideo: null }
 
   onSearchSubmit = async term => {
     const res = await youtube.get("/search", {
@@ -18,11 +18,18 @@ class App extends React.Component {
     this.setState({ videos: res.data.items })
   }
 
+  onVideoSelect = video => {
+    console.log("we got clicked App", video)
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     )
   }
