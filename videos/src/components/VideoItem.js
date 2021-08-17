2aaec0e29a9e53312f9537f26ce47b0aabbde9f5
skeown13 <code@keown.net>
2021-08-17 12:53:46 -0700

    created a component that returns a div with a title and thumbnail of a video taken from the youtube api

---
+++ videos/src/components/VideoItem.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const VideoItem = ({ video }) => {
+  return (
+    <div>
+      <img
+        alt={video.snippet.description}
+        src={video.snippet.thumbnails.medium.url}
+      />
+      {video.snippet.title}
+    </div>
+  )
+}
+
+export default VideoItem
