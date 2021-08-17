f96497af798055ba8fe0413aac388a03bfea85ae
skeown13 <code@keown.net>
2021-08-17 13:06:56 -0700

    created the onSearchSubmit function which does the axios call to the youtube api and sets the state for videos to be used in child components. Also rendering the SearchBar and VideoList to the page.

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,20 +1,28 @@
 import React from "react"
+import youtube from "../api/youtube"
 import SearchBar from "./SearchBar"
-import axios from "axios"
+import VideoList from "./VideoList"
 
 class App extends React.Component {
   state = { videos: [] }
 
-  onSearchSubmit = () => {
-    axios.get().then(res => {
-      console.log(res)
+  onSearchSubmit = async term => {
+    const res = await youtube.get("/search", {
+      params: {
+        maxResults: 5,
+        q: term,
+      },
     })
+
+    console.log(res)
+    this.setState({ videos: res.data.items })
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
