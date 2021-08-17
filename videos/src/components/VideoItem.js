3dc6b6c89e7225d12a96e922d3a4dad9abae6601
skeown13 <code@keown.net>
2021-08-17 14:26:13 -0700

    small bit of css styling. destructured onVideoSelect from props and used it in an onClick function to pass in the video that is being selected

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,13 +1,15 @@
+import "./VideoItem.css"
 import React from "react"
 import { Item } from "semantic-ui-react"
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <Item.Group>
+    <Item.Group onClick={() => onVideoSelect(video)} className="video-item">
       <Item>
         <Item.Image
           alt={video.snippet.description}
           src={video.snippet.thumbnails.medium.url}
+          className="video-image"
         />
         <Item.Content verticalAlign="middle">
           <Item.Header as="a">{video.snippet.title}</Item.Header>
