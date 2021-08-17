0d2e6ebe33939232eea12ce59556e6b1c694300b
skeown13 <code@keown.net>
2021-08-17 12:55:09 -0700

    updated component to use Semantic-UI item styling

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,14 +1,19 @@
 import React from "react"
+import { Item } from "semantic-ui-react"
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      <img
-        alt={video.snippet.description}
-        src={video.snippet.thumbnails.medium.url}
-      />
-      {video.snippet.title}
-    </div>
+    <Item.Group>
+      <Item>
+        <Item.Image
+          alt={video.snippet.description}
+          src={video.snippet.thumbnails.medium.url}
+        />
+        <Item.Content verticalAlign="middle">
+          <Item.Header as="a">{video.snippet.title}</Item.Header>
+        </Item.Content>
+      </Item>
+    </Item.Group>
   )
 }
 
