2c257acea03c5331b4d9a27dc3be9e1db5e0cabe
skeown13 <code@keown.net>
2021-08-18 10:03:56 -0700

    used SemanticUI to style/create an accordion and used hooks to include state to decide which accordion is active and open

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Accordion, Icon } from "semantic-ui-react";
 
-const Accordion = () => {
-  return <h1>Accordion</h1>;
+const AccordionComponent = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    return (
+      <Container key={item.title}>
+        <Accordion fluid styled>
+          <Accordion.Title
+            active={activeIndex === index}
+            index={index}
+            onClick={() => setActiveIndex(index)}
+          >
+            <Icon name="dropdown" />
+            {item.title}
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === index}>
+            <p>{item.content}</p>
+          </Accordion.Content>
+        </Accordion>
+      </Container>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
 };
 
-export default Accordion;
+export default AccordionComponent;
