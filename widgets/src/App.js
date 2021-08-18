4f25bf55b25b1311229b3e4e9cfc0d5735c1a8c7
skeown13 <code@keown.net>
2021-08-18 08:27:34 -0700

    created a hardcoded list of items and passed them as props to the Accordion component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React",
+    content: "You use React by creating components",
+  },
+];
 
 const App = () => {
-  return <div>Gonna Make an Accordion</div>;
+  return (
+    <div>
+      <h1>Gonna Make an Accordion</h1>
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default App;
