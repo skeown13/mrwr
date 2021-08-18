db0d8f09b942bd9e7f20362da9b9a8b0e320d561
skeown13 <code@keown.net>
2021-08-18 07:58:07 -0700

    added the VideoDetail to the page. Used SemantiUI Grid system to style the page. Added a componentDidMount to automatically search for a default term.

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react"
+import { Grid } from "semantic-ui-react"
 import youtube from "../api/youtube"
 import SearchBar from "./SearchBar"
 import VideoList from "./VideoList"
+import VideoDetail from "./VideoDetail"
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null }
+
+  componentDidMount() {
+    this.onSearchSubmit("puppies")
+  }
 
   onSearchSubmit = async term => {
     const res = await youtube.get("/search", {
@@ -15,21 +21,31 @@ class App extends React.Component {
     })
 
     console.log(res)
-    this.setState({ videos: res.data.items })
+    this.setState({ videos: res.data.items, selectedVideo: res.data.items[0] })
   }
 
   onVideoSelect = video => {
     console.log("we got clicked App", video)
+    this.setState({ selectedVideo: video })
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column width={11}>
+              <VideoDetail selectedVideo={this.state.selectedVideo} />
+            </Grid.Column>
+            <Grid.Column width={5}>
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     )
   }
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoItem from "./VideoItem"
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const myVideos = videos.map(video => {
-    return <VideoItem video={video} onVideoSelect={onVideoSelect} />
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    )
   })
 
   return <div>{myVideos}</div>
