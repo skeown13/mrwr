b6cc9b11656189d26989e151b39fc1bc8035f32a
skeown13 <code@keown.net>
2022-01-03 07:52:00 -0800

    included setTimeout & clearTimeout to delay our search from occuring immediately

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -22,8 +22,18 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    if (term) {
+    if (term && !results.length) {
       search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
     }
   }, [term]);
 
